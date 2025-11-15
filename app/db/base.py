42e06c23a42c546cd4c05bbe0989658648cb937a
AaKaSh0507 <try.aakashm@gmail.com>
2025-11-15 22:22:29 +0530

    models for course, tenant and users table

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,11 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import declarative_base, declared_attr
 
-Base = declarative_base()
+
+class CustomBase:
+    @declared_attr
+    def id(cls):
+        return Column(Integer, primary_key=True, index=True)
+
+
+Base = declarative_base(cls=CustomBase)
